7f0dc40ff7a5552dad8643b73a9e7784ec5c3d2f
Oleksandr <96943240+Alex19919@users.noreply.github.com>
2022-08-09 22:02:03 +0300

    NEWONE

--- popularity.py
+++ popularity.py
@@ -13,7 +13,33 @@
 # Якщо слова мають однакову популярність, сортування повинно бути виконано за абеткою.
 # Приклади:
 
-popularity('apple kiwi pineapple kiwi apple kiwi') -> ['kiwi', 'apple', 'pineapple']
-popularity('aPPle pine Apple kiwi Apple kiwi') -> ['apple', 'kiwi', 'pine']
-popularity('aPPle pine Apple kiwi Apple kiwi') -> ['apple', 'kiwi', 'pine']
-popularity('aab aaa aac aab aac aaa x') -> ['aaa', 'aab', 'aac', 'x']
+
+def popularity(text):
+    # регістр(lovercase).
+    lower_text = text.lower()
+    words_arr = lower_text.split(' ')
+    words_counter = dict()
+    for word in words_arr:
+        if word in words_counter:
+            words_counter[word] += 1
+        else:
+            words_counter[word] = 1
+
+    # def get_second_key(item):
+    #     return item[1]
+
+    # def get_first_key(item):
+    #     return item[0]
+
+    # words_counter.items()  # -> [(apple, 1), (kiwi, 1), (pineapple, 1)]
+
+    # sorted_by_words = sorted(words_counter.items(), key=lambda item: item[0])
+
+    # sorted_by_popularity = sorted(sorted_by_word, key=get_second_key,
+
+
+
+print(popularity('apple kiwi pineapple kiwi apple kiwi')) -> ['kiwi', 'apple', 'pineapple']
+print(popularity('aPPle pine Apple kiwi Apple kiwi')) -> ['apple', 'kiwi', 'pine']
+print(popularity('aPPle pine Apple kiwi Apple kiwi')) -> ['apple', 'kiwi', 'pine']
+print(popularity('aab aaa aac aab aac aaa x')) -> ['aaa', 'aab', 'aac', 'x']
